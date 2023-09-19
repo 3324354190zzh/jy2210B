66c1b3d8787e0daf1e523bb538bbd37b988a9140
3324354190zzh <3324354190@qq.com>
2023-09-19 20:02:24 +0800

    第一次启动

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<div class=''>
+  aaaa
+</div>
 </template>
+
+<script setup lang='ts'>
+</script>
+
+<style lang='scss' scoped>
+</style>
